b90b177a748aac0ca3019f7d41dc8a665e96623b
Waterbug <amoseman18@gmail.com>
2020-12-03 13:52:11 -0700

    finished app.js code except for fs writefile function, tests all pass, but it wont run the prompts in node

--- Develop/app.js
+++ Develop/app.js
@@ -5,108 +5,145 @@ const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
-const OUTPUT_DIR = path.resolve(__dirname, "output");
+const OUTPUT_DIR = path.resolve(__dirname, "output");                       //outputPath
 const outputPath = path.join(OUTPUT_DIR, "team.html"); // our writefile file, name of the file, data is the render method with employee array passed into it
 
 const render = require("./lib/htmlRenderer");
 const { listenerCount } = require("process");
+
+//created empty array for employees to get pushed to
 const employeeArr = []
 
- function createManager() {
-    inquirer.prompt ([
+//takes the data from the input and pushes the newly created employee role to the array above 
+function createManager() {
+    inquirer.prompt([
 
-    ]) .then((answers) => {  ///make sure order is correct
+        {
+            type: "input",
+            name: "name",
+            message: "What is your name?",
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is your id?",
+
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is your email?",
+        },
+        {
+            type: "input",
+            name: "officeNumber",
+            message: "What is your office number?"
+        },
+
+    ]).then((answers) => {   //create new manager object on employee class with the users input
         const manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
         employeeArr.push(manager);
         addTeamMember();
     })
 
-}
+};
+
 function createIntern() {
-    inquirer.prompt ([
+    inquirer.prompt([
 
-    ])
+        {
+            type: "input",
+            name: "name",
+            message: "What is the intern's name?",
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is their id?",
 
-}
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is their email?",
+        },
+        {
+            type: "input",
+            name: "school",
+            message: "What is the name of their school?",
+        },
+
+    ]).then((answers) => {
+        const intern = new Intern(answers.name, answers.id, answers.email, answers.school);
+        employeeArr.push(intern);
+        addTeamMember();
+    })
+
+};
+
 function createEngineer() {
-    inquirer.prompt ([
-        
-    ])
+    inquirer.prompt([
 
-}
+        {
+            type: "input",
+            name: "name",
+            message: "What is the engineer's name?",
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is their id?",
+
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is their email?",
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "What is their Github username?",
+        },
+
+    ]).then((answers) => {
+        const engineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
+        employeeArr.push(engineer);
+        addTeamMember();
+    })
+
+};
+
 function addTeamMember() {
-    inquirer.prompt ([
+    inquirer.prompt([
+
         {
             type: "list",
             name: "employeeRole",
             message: "What type of employee would you like to add?",
-            choices: ["Intern", "Engineer", "None"]
+            choices: ["Intern", "Engineer", "Manager", "None"]
 
         }
 
-    ]) . then((answers) => {
+    ]).then((answers) => {
         if (answers.employeeRole === "Intern") {
-            createIntern()
-        } //none --> build team function
+            createIntern();
+        } else if (answers.employeeRole === "Engineer") {
+            createEngineer();
+        } else if (answers.employeeRole === "Manager") {
+            createManager();   ///is this necessary?
+        } else { buildTeam() };
     })
-   }
+};
 
-
-   inquirer.prompt([
-    {
-        type: "input",
-        name: "name",
-        message: "What is your name?",
-    },
-    {
-        type: "input",
-        name: "id",
-        message: "What is your id?",
-
-    },
-    {
-        type: "input",
-        name: "email",
-        message: "What is your email?",
-    },
-    {
-        type: "list",
-        name: "role",
-        message: "What is your role in the company?",
-        choices: ["Intern", "Engineer", "Manager"],
-    },
-    {
-        type: "input",
-        name: "school",
-        message: "What is the name of your school?",
-    },
-    {
-        type: "input",
-        name: "github",
-        message: "What is your Github username?",
-    },
-    {
-        type: "input",
-        name: "officeNumber",
-        message: "What is your office number?"
-    },
-    
-]);
-
-.then(answers => {
-    // Use user feedback for... whatever!!
-})
-    .catch(error => {
-        if (error.isTtyError) {
-            // Prompt couldn't be rendered in the current environment
-        } else {
-            // Something else when wrong
-        }
-    });
-
-    function buildTeam() {
-        //call render and passs in info with writefile
-    }
+function buildTeam() {
+    //const outputPath = path.join(OUTPUT_DIR, "team.html"); // our writefile file, name of the file, data is the render method with employee array passed into it
+    //call render and passs in info with writefile
+    //filepath stored in a var  //data being put in file
+    fs.writeFile(outputPath, render(employeeArr), (err) => {
+        if (err) throw err;
+    })
+}
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
